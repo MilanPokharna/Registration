03fe081940df30ef4a7bc871539fbbdbd061c5c7
MiLaN <milan.pokharna1998@gmail.com>
2018-09-15 18:53:29 +0530

    initial commit

--- app/src/main/java/com/android/registration/LoginActivity.java
+++ app/src/main/java/com/android/registration/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.android.registration;
+Kpackage com.android.registration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.android.registration.R;
 
 public class LoginActivity extends AppCompatActivity {
 
