96a89724e2652ecc951f858460465619f577481d
MiLaN <milan.pokharna1998@gmail.com>
2018-09-15 19:43:44 +0530

    initial commit

--- app/src/main/java/com/android/registration/LoginActivity.java
+++ app/src/main/java/com/android/registration/LoginActivity.java
@@ -1,15 +1,163 @@
-Kpackage com.android.registration;
+package com.android.registration;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.android.registration.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.loginButton)
+    Button loginButton;
+    private FirebaseAuth mAuth;
+    public FirebaseUser user;
+    private GoogleApiClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                }).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+
     }
+
+    @OnClick(R.id.loginButton)
+    public void onViewClicked() {
+
+
+    }
+
+        public void signIn() {
+
+            mGoogleSignInClient.clearDefaultAccountAndReconnect();
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+
+        }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            final FirebaseUser user = mAuth.getCurrentUser();
+                            final String a = user.getUid().toString();
+                            final String url = user.getPhotoUrl().toString();
+                            if (url != null) {
+
+
+
+                            }
+                        } else {
+//                            Snackbar snackbar1 = Snackbar.make(startActivityLayout, "Login with College ID", Snackbar.LENGTH_SHORT);
+//                            snackbar1.show();
+
+                        }
+                    }
+                });
+//
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if(user != null)
+        {
+            Intent i = new Intent(LoginActivity.this,LoginActivity.class);
+            startActivity(i);
+        }
+    }
+
+
+    private void launchHomeScreen() {
+
+        if (isNetworkConnected()) {
+
+            updateUI(user);
+        } else {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(StartActivity.this);
+            dialog.setTitle("Connectino Error ");
+            dialog.setCancelable(false);
+            dialog.setMessage("Unable to connect with the server.\n Check your Internet connection and try again.");
+            dialog.setPositiveButton("TRY AGAIN", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    launchHomeScreen();
+                }
+            }).show();
+        }
+    }
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null;
+    }
+
 }
